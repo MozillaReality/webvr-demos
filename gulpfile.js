0744bd227f758f971bac88e1a3380325c6558339
Casey Yee <caseyyee.ca@gmail.com>
2015-07-16 11:06:26 -0700

    update demo path

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var connect = require('gulp-connect');
 var download = require('gulp-download');
 
-const WEB_ROOT = './demos/';
+const WEB_ROOT = './';
 
 gulp.task('syncmedia', function() {
   download('http://mozvr.com/projects/polarsea/content/videos/PolarSeaTrailer-3.mp4')
