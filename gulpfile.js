3017857949e959cee540df0d04c84210f52d17e8
chris van wiemeersch <cvan@users.noreply.github.com>
2016-02-19 12:49:40 -0800

    use s3 bucket URLs for Polar Sea Videos (fixes #18)
    also removed `nepal.mp4` because we already have [`nepal.webm`](https://github.com/MozVR/webvr-demos/blob/master/demos/nepal/videos/nepal.webm) checked into the repo (since it's smaller than the 100 MB limit). and the http://mozvr.com/projects/nepal/videos/nepal.mp4 URL doesn't even work anymore (the Nepal videos aren't in the http://mozvr.com.s3.amazonaws.com/ bucket anymore).

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,10 @@ var download = require('gulp-download');
 const WEB_ROOT = './';
 
 gulp.task('syncmedia', function() {
-  download('http://mozvr.com/projects/polarsea/content/videos/PolarSeaTrailer-3.mp4')
+  download('http://mozvr.com.s3.amazonaws.com/videos/PolarSeaTrailer-3.mp4')
     .pipe(gulp.dest('./demos/polarsea/videos/'));
-  download('http://mozvr.com/projects/nepal/videos/nepal.mp4')
-    .pipe(gulp.dest('./demos/nepal/videos/'));
+  download('http://mozvr.com.s3.amazonaws.com/videos/PolarSeaTrailer-3.webm')
+    .pipe(gulp.dest('./demos/polarsea/videos/'));
 });
 
 gulp.task('webserver', function() {
